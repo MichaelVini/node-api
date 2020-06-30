ad2a329a20776e7906db3c9bf292ce81887f3328
Michael <michaelvini62@gmail.com>
2020-06-30 20:09:46 -0300

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // Inicia o app
 const app = express();
+
 //Permitir o envio de dados para aplicalçao em formato de json
 app.use(express.json());
+// Permitir que outros endereços acessem a api
+app.use(cors());
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true,
@@ -15,5 +19,4 @@ requireDir('./src/models');
 
 //Rotas
 app.use('/api', require("./src/routes"));
-
 app.listen(3001);
